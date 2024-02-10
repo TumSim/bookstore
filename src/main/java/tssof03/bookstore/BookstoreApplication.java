9ebc35c279224dc0e7dfea6923199e84610cec1e
TumSim <tuomas.simoinen@gmail.com>
2024-02-10 15:50:00 +0200

    booklist page added

--- src/main/java/tssof03/bookstore/BookstoreApplication.java
+++ src/main/java/tssof03/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package tssof03.bookstore;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import tssof03.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
@@ -20,9 +23,11 @@ public class BookstoreApplication {
 		return (args) -> {
 			// Your code...add some demo data to db
 
+			log.info("couple books");
 			Book book1 = new Book("tuomas", "Simoinen", 2008, "1995", 20.5);
 			Book book2 = new Book("ida", "Hanninen", 2019, "1998", 1000.50);
 
+			log.info("fetch");
 			BookRepository.save(book1);
 			BookRepository.save(book2);
 
--- src/main/java/tssof03/bookstore/web/BookController.java
+++ src/main/java/tssof03/bookstore/web/BookController.java
@@ -1,15 +1,25 @@
 package tssof03.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import tssof03.bookstore.domain.BookRepository;
+
+
 
 @Controller
 
 public class BookController {
+    
+    @Autowired
+    private BookRepository bookRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/booklist")
 
-    public String firstPage(){
-        return "frontpage";
+    public String firstPage(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist"; //booklist.html
     }
 }
